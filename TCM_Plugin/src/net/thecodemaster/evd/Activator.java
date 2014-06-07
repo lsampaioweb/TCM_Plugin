3a931e09bac06092af2a5443948e992c42f58a1d
Luciano Sampaio <lsampaioweb@gmail.com>
2014-06-07 11:00:53 -0300

    OPEN - task 41: Create the deployment infrastructure of the plug-in https://github.com/lsampaioweb/TCM_Plugin/issues/issues/issue/41
    Created some l10n.

--- TCM_Plugin/src/net/thecodemaster/evd/Activator.java
+++ TCM_Plugin/src/net/thecodemaster/evd/Activator.java
@@ -21,7 +21,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID.
-	public static final String	PLUGIN_ID	= "THE_CODE_MASTER_EARLY_VULNERABILITY_DETECTOR"; //$NON-NLS-1$
+	public static final String	PLUGIN_ID	= "tcm.early.vulnerability.detector"; //$NON-NLS-1$
 
 	// The shared instance.
 	private static Activator		plugin;
