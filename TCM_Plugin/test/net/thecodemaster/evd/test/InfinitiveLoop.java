190092efd23bf35c9febafeda5a5101b4903ed51
Luciano Sampaio <lsampaioweb@gmail.com>
2014-07-03 09:59:19 -0300

    OPEN - task 45: Work on methods from objects/classes https://github.com/lsampaioweb/TCM_Plugin/issues/issues/issue/45
    Created more tests for Inifinitive Loops and InnerClasses.

--- TCM_Plugin/test/net/thecodemaster/evd/test/InfinitiveLoop.java
+++ TCM_Plugin/test/net/thecodemaster/evd/test/InfinitiveLoop.java
@@ -10,16 +10,13 @@ import org.eclipse.core.resources.IResource;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class FromOtherClasses extends AbstractTestVerifier {
+public class InfinitiveLoop extends AbstractTestVerifier {
 
 	@Override
 	protected List<IResource> getResources() {
 		Map<String, List<String>> resourceNames = Creator.newMap();
 
-		resourceNames.put(AbstractTestVerifier.PACKAGE_BASE, newList("Animal.java"));
-		resourceNames.put(AbstractTestVerifier.PACKAGE_OTHER_PACK, newList("Animal.java"));
-		resourceNames.put(AbstractTestVerifier.PACKAGE_SERVLET,
-				newList("Person.java", "Employee.java", "FromOtherClasses.java"));
+		resourceNames.put(AbstractTestVerifier.PACKAGE_SERVLET, newList("InfinitiveLoop.java"));
 
 		return getRersources(resourceNames);
 	}
