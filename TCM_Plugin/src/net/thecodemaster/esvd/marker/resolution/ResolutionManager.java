273a5698395f364f43a1970e56d35c9c0ef2580d
icarocarlosandrade <icarocarlosandrade@gmail.com>
2015-04-29 11:57:59 -0300

    Added ResolutionManager

---
+++ TCM_Plugin/src/net/thecodemaster/esvd/marker/resolution/ResolutionManager.java
@@ -0,0 +1,50 @@
+package net.thecodemaster.esvd.marker.resolution;
+
+import org.eclipse.ui.IMarkerResolution;
+
+public class ResolutionManager {
+
+	protected IMarkerResolution[] getCommandInjectionResolutions() {
+		return null;
+	}
+
+	protected IMarkerResolution[] getCookiePoisoningResolutions() {
+		return null;
+	}
+
+	protected IMarkerResolution[] getCrossSiteScriptingResolutions() {
+		return null;
+	}
+
+	protected IMarkerResolution[] getHTTPResponseSplittingResolutions() {
+		return null;
+	}
+
+	protected IMarkerResolution[] getLDAPInjectionResolutions() {
+		return null;
+	}
+
+	protected IMarkerResolution[] getLogForgingResolutions() {
+		return null;
+	}
+
+	protected IMarkerResolution[] getPathTraversalResolutions() {
+		return null;
+	}
+
+	protected IMarkerResolution[] getReflectionInjectionResolutions() {
+		return null;
+	}
+
+	protected IMarkerResolution[] getSecurityMisconfigurationResolutions() {
+		return null;
+	}
+
+	protected IMarkerResolution[] getSQLInjectionResolutions() {
+		return null;
+	}
+
+	protected IMarkerResolution[] getXPathInjectionResolutions() {
+		return null;
+	}
+}
